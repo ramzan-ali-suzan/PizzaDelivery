29409ea17295fa0de53fcce953e15dd306605532
Ramzan <ramzan.ali.suzan@gmail.com>
2020-04-09 10:57:35 +0600

    Add PizzaController

---
+++ PizzaDelivery/Controllers/PizzaController.cs
@@ -0,0 +1,26 @@
+﻿using PizzaDelivery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PizzaDelivery.Controllers
+{
+    [RoutePrefix("api/pizza")]
+    public class PizzaController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PizzaController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public IEnumerable<Pizza> Get()
+        {
+            return _context.Pizzas;
+        }
+    }
+}
