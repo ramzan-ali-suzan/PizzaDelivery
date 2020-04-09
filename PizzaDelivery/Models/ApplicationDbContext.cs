fa09854b4488417d9e4e0ab5224d0856506bd568
Ramzan <ramzan.ali.suzan@gmail.com>
2020-04-09 10:52:09 +0600

    Enable Migrations

---
+++ PizzaDelivery/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PizzaDelivery.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+
+        }
+    }
+}
